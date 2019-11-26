c4a5a42bfacc6d43120c7992636e3498e8259e71
Alexander Harnisch <harnisc6@dev-intel14.i>
2019-11-26 17:24:46 -0500

    Homework 6:     Bucketsort v1 and v2 done.

--- homework/6/bucket_sort_v1.c
+++ homework/6/bucket_sort_v1.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
+#include <float.h>
+
+#define ARRAY_TAG 1
 
 
 // Comparison function used by qsort
@@ -11,6 +15,7 @@ int compare_dbls(const void* arg1, const void* arg2)
     else if (a1 == a2) return 0;
     else return 1;
 }
+
 // Sort the array in place
 void qsort_dbls(double *array, int array_len)
 {
@@ -18,71 +23,192 @@ void qsort_dbls(double *array, int array_len)
           compare_dbls);
 }
 
-void main(int argc, char* argv[]){
+// Get the maximum, minimum and average value in a array of floats
+void get_min_avg_max(double *array, int n, double *min, double *avg, double *max)
+{
+    *max = 0.; *min = DBL_MAX; *avg = 0.;
+    for (int i = 0; i < n; i++)
+    {
+        if ( array[i] > *max )
+            *max = array[i];
+        if ( array[i] < *min )
+            *min = array[i];
+        *avg += array[i];
+    }
+    *avg /= n;
+}
+
+int main(int argc, char* argv[])
+{
 
     double *input_array;   // the input array
     double *bucketlist;    //this array will contain input elements in order of the processors
                             //e.g elements of process 0 will be stored first, then elements of process 1, and so on
     double *local_array;    //This array will contain the elements in each process
+    int local_count;
 
-    int n;
-    int p;
-    int i;
-    int my_rank;
+    int n, p, i, my_rank;
 
     int *scounts;           //This array will contain the counts of elements each processor will receive
     int *dspls;             //The relative offsets in bucketlist array where the elements of different processes
                             //will be stored
     int *bin_elements;       //it will keep track of how many elements have been included in the pth bin
 
-    n = atoi(argv[1]);
+    double t_total;
+    double t_generate;
+    double t_binning;
+    double t_distribute;
+    double t_local_sort;
+    double t_gathering;
 
-    p = 4;                  //dummy processor count, need to set it using MPI_comm_size
+    MPI_Init(NULL, NULL);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
 
-    input_array = malloc(n*sizeof(double));
-    bucketlist = malloc(n*sizeof(double));
     scounts = malloc(p*sizeof(int));
-    dspls = malloc(p*sizeof(int));
-    bin_elements = malloc(p*sizeof(int));
 
-    for(i = 0 ; i < n ; i++){
-        input_array[i] = ((double) rand()/RAND_MAX);
+    if ( my_rank == 0 )
+    {
+        t_total = MPI_Wtime();
+        t_generate = MPI_Wtime();
+        if ( argc > 1 )
+            n = atoi(argv[1]);
+        else
+            n = 100000000;
+        input_array = malloc(n*sizeof(double));
+
+        bucketlist = malloc(n*sizeof(double));
+        dspls = malloc(p*sizeof(int));
+        bin_elements = malloc(p*sizeof(int));
+
+        for ( i = 0; i < p; i++ )
+            scounts[i] = 0 ;
+
+        for ( i = 0; i < n; i++ )
+        {
+            input_array[i] = ((double) rand()/( RAND_MAX + 1.));
+            scounts[(int)(input_array[i]/(1.0/p))]++;
+        }
+
+        t_binning = MPI_Wtime();
+        t_generate = t_binning - t_generate;
+
+        for ( i = 0; i < p; i++ )
+            bin_elements[i] = scounts[i];
+
+        dspls[0] = 0;
+        for ( i = 0; i < p-1; i++ )
+            dspls[i+1] = dspls[i] + scounts[i];
+
+        int bin, pos;
+        for ( i = 0; i < n; i++ )
+        {
+            bin = (int)(input_array[i]/(1.0/p));
+            pos = dspls[bin] + scounts[bin] - bin_elements[bin];
+            bucketlist[pos] = input_array[i];
+            bin_elements[bin]--;
+        }
+        t_binning = MPI_Wtime() - t_binning;
+
+        // The input array is not needed anymore so let's free it to save
+        // memory. The output will be the bucketlist array.
+        free(input_array);
+
+        /*MPI_Request req;*/
+        /*for ( i = 1; i < p; i++ )*/
+        /*{*/
+            /*MPI_Isend(&bucketlist[dspls[i]], scounts[i], MPI_DOUBLE, i,*/
+                    /*ARRAY_TAG, MPI_COMM_WORLD, &req);*/
+            /*MPI_Request_free(&req);*/
+        /*}*/
+
+        // no need to reallocate memory for the root process, we'll just use the
+        // bucketlist array in-place to save some memory
+        local_array = bucketlist;
     }
 
-    for(i = 0 ; i < p ; i++){
-        scounts[i] = 0 ;
+    /*if ( my_rank > 0 )*/
+    /*{*/
+        /*MPI_Status status;*/
+        /*MPI_Probe(0, ARRAY_TAG, MPI_COMM_WORLD, &status);*/
+        /*MPI_Get_elements(&status, MPI_DOUBLE, &local_count);*/
+        /*local_array = malloc(local_count*sizeof(double));*/
+        /*MPI_Recv(local_array, local_count, MPI_DOUBLE, 0, ARRAY_TAG, MPI_COMM_WORLD, &status);*/
+    /*}*/
+
+    // broadcast the bin sizes to prepare local arrays for the scattering
+    // operation
+    if ( my_rank == 0 )
+        t_distribute = MPI_Wtime();
+    MPI_Bcast(scounts, p, MPI_INT, 0, MPI_COMM_WORLD);
+    local_count = scounts[my_rank];
+
+    // no need to reallocate memory for the root process, we'll just use the
+    // bucketlist array in-place to save some memory
+    if ( my_rank > 0 )
+        local_array = malloc(local_count*sizeof(double));
+    MPI_Scatterv(bucketlist, scounts, dspls, MPI_DOUBLE, local_array,
+            local_count, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    if ( my_rank == 0 )
+        t_distribute = MPI_Wtime() - t_distribute;
+
+    // do the local sorting
+    t_local_sort = MPI_Wtime();
+    qsort_dbls(local_array, local_count);
+    t_local_sort = MPI_Wtime() - t_local_sort;
+
+    // combine the results
+    if ( my_rank == 0 )
+        t_gathering = MPI_Wtime();
+    MPI_Gatherv(local_array, local_count, MPI_DOUBLE, bucketlist,
+            scounts, dspls, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+    if ( my_rank == 0 )
+        t_gathering = MPI_Wtime() - t_gathering;
+
+    double *t_local_sort_array;
+    if ( my_rank == 0 )
+    {
+        t_total = MPI_Wtime() - t_total;
+        t_local_sort_array = malloc(p*sizeof(double));
     }
+    MPI_Gather(&t_local_sort, 1, MPI_DOUBLE, t_local_sort_array, 1, MPI_DOUBLE,
+            0, MPI_COMM_WORLD);
+    MPI_Finalize();
 
-    //counting the elements in each processor
-    for(i = 0 ; i < n ; i++){
-        scounts[(int)(input_array[i]/(1.0/p))]++;
+    // Check result
+    if ( my_rank == 0 )
+    {
+        /*{*/
+            /*printf("Output is: [");*/
+            /*for ( i = 0; i < n - 1; i++ )*/
+                /*printf("%f, ", bucketlist[i]);*/
+            /*printf("%f]\n", bucketlist[n-1]);*/
+        /*}*/
+
+        // Only print someting here if the output is incorrect, I don't want
+        // spam in my output files
+        for ( i = 0; i < n - 1; i++ )
+            if ( bucketlist[i] > bucketlist[i+1] )
+            {
+                printf("OUTPUT IS INCORRECT!\n");
+                return 1;
+            }
+
+        double local_sort_min, local_sort_avg, local_sort_max;
+        get_min_avg_max(t_local_sort_array, p, &local_sort_min,
+                &local_sort_avg, &local_sort_max);
+
+        printf("Time Report:\n");
+        printf("============\n");
+        printf("Total:      %.6f\n", t_total);
+        printf("Generate:   %.6f\n", t_generate);
+        printf("Binning:    %.6f\n", t_binning);
+        printf("Distribute: %.6f\n", t_distribute);
+        printf("Local Sort:\n");
+        printf("       Min: %.6f\n", local_sort_min);
+        printf("       Avg: %.6f\n", local_sort_avg);
+        printf("       Max: %.6f\n", local_sort_max);
+        printf("Gathering:  %.6f\n", t_gathering);
     }
-
-    for(i = 0 ; i<p ; i++){
-        bin_elements[i] = scounts[i];
-    }
-
-    dspls[0] = 0;
-    for(i = 0 ; i< p-1 ;i++){
-        dspls[i+1] = dspls[i] + scounts[i];
-    }
-
-    int bin;
-    int pos;
-    for(i = 0 ; i < n ; i++){
-        bin = (int)(input_array[i]/(1.0/p));
-        pos = dspls[bin] + scounts[bin] - bin_elements[bin];
-        bucketlist[pos] = input_array[i];
-        bin_elements[bin]--;
-    }
-
-    /*
-     Each bucket can now be sorted individually, either using a different sorting algorithm (i.e., qsort_dbls())
-     as you are asked to do, or by recursively applying the bucket sorting algorithm, but we leave this part for
-     the parallel code.
-     Note that the scount array contains the number of elements each process will receive from the root, dspls
-     contains the relative offsets where elements from different processes will be stored, and bucketlist array
-     contains the elements stored in the order of the processors.
-     */
-
+    return 0;
 }
--- homework/6/bucket_sort_v2.c
+++ homework/6/bucket_sort_v2.c
@@ -1,5 +1,10 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <mpi.h>
+#include <math.h>
+#include <float.h>
+
+#define ARRAY_TAG 1
 
 
 // Comparison function used by qsort
@@ -11,6 +16,7 @@ int compare_dbls(const void* arg1, const void* arg2)
     else if (a1 == a2) return 0;
     else return 1;
 }
+
 // Sort the array in place
 void qsort_dbls(double *array, int array_len)
 {
@@ -18,71 +24,230 @@ void qsort_dbls(double *array, int array_len)
           compare_dbls);
 }
 
-void main(int argc, char* argv[]){
+// Get the maximum, minimum and average value in a array of floats
+void get_min_avg_max(double *array, int n, double *min, double *avg, double *max)
+{
+    *max = 0.; *min = DBL_MAX; *avg = 0.;
+    for (int i = 0; i < n; i++)
+    {
+        if ( array[i] > *max )
+            *max = array[i];
+        if ( array[i] < *min )
+            *min = array[i];
+        *avg += array[i];
+    }
+    *avg /= n;
+}
 
-    double *input_array;   // the input array
-    double *bucketlist;    //this array will contain input elements in order of the processors
+int main(int argc, char* argv[])
+{
+
+    double *result;         // This array will contain the result gathered at the root process
+    double *bucketlist;     //this array will contain input elements in order of the processors
                             //e.g elements of process 0 will be stored first, then elements of process 1, and so on
     double *local_array;    //This array will contain the elements in each process
 
-    int n;
-    int p;
-    int i;
-    int my_rank;
+    int n, p, i, my_rank;
 
     int *scounts;           //This array will contain the counts of elements each processor will receive
     int *dspls;             //The relative offsets in bucketlist array where the elements of different processes
                             //will be stored
-    int *bin_elements;       //it will keep track of how many elements have been included in the pth bin
+    int *bin_elements;      //it will keep track of how many elements have been included in the pth bin
 
-    n = atoi(argv[1]);
+    double t_total;
+    double t_generate;
+    double t_binning;
+    double t_distribute;
+    double t_local_sort;
+    double t_gathering;
 
-    p = 4;                  //dummy processor count, need to set it using MPI_comm_size
 
-    input_array = malloc(n*sizeof(double));
-    bucketlist = malloc(n*sizeof(double));
+    MPI_Init(&argc, &argv);
+    MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
+    MPI_Comm_size(MPI_COMM_WORLD, &p);
+
     scounts = malloc(p*sizeof(int));
+
+    t_generate = MPI_Wtime();
+    if ( my_rank == 0 )
+        t_total = t_generate;
+
+    if ( argc > 1 )
+        n = atoi(argv[1]);
+    else
+        n = 100000000;
+
+    const int LOCAL_N = ceil(( (float) n )/p);
+    const int CHUNK_SZ = ceil(1./p);
+
+    n = p*LOCAL_N;
+    bucketlist = malloc(n*sizeof(double));
     dspls = malloc(p*sizeof(int));
     bin_elements = malloc(p*sizeof(int));
+    scounts = malloc(p*sizeof(int));
+    local_array = malloc(LOCAL_N*sizeof(double));
 
-    for(i = 0 ; i < n ; i++){
-        input_array[i] = ((double) rand()/RAND_MAX);
+    for ( i = 0; i < p; i++ )
+        scounts[i] = 0;
+
+    for ( i = 0 ; i < LOCAL_N ; i++ )
+    {
+        local_array[i] = ((double) rand()/( RAND_MAX + 1.));
+        scounts[(int)(local_array[i]/(1.0/p))]++;
     }
 
-    for(i = 0 ; i < p ; i++){
-        scounts[i] = 0 ;
-    }
+    t_binning = MPI_Wtime();
+    t_generate = t_binning - t_generate;
 
-    //counting the elements in each processor
-    for(i = 0 ; i < n ; i++){
-        scounts[(int)(input_array[i]/(1.0/p))]++;
-    }
-
-    for(i = 0 ; i<p ; i++){
+    for ( i = 0 ; i<p ; i++ )
         bin_elements[i] = scounts[i];
-    }
 
     dspls[0] = 0;
-    for(i = 0 ; i< p-1 ;i++){
+    for ( i = 0; i< p-1; i++ )
         dspls[i+1] = dspls[i] + scounts[i];
-    }
 
-    int bin;
-    int pos;
-    for(i = 0 ; i < n ; i++){
-        bin = (int)(input_array[i]/(1.0/p));
+    int bin, pos;
+    for ( i = 0; i < LOCAL_N; i++ )
+    {
+        bin = (int)(local_array[i]/(1.0/p));
         pos = dspls[bin] + scounts[bin] - bin_elements[bin];
-        bucketlist[pos] = input_array[i];
+        bucketlist[pos] = local_array[i];
         bin_elements[bin]--;
     }
 
-    /*
-     Each bucket can now be sorted individually, either using a different sorting algorithm (i.e., qsort_dbls())
-     as you are asked to do, or by recursively applying the bucket sorting algorithm, but we leave this part for
-     the parallel code.
-     Note that the scount array contains the number of elements each process will receive from the root, dspls
-     contains the relative offsets where elements from different processes will be stored, and bucketlist array
-     contains the elements stored in the order of the processors.
-     */
+    t_distribute = MPI_Wtime();
+    t_binning = t_distribute - t_binning;
 
+    free(local_array);
+
+    int *rcounts, *rdspls;
+    rcounts = malloc(p*sizeof(int));
+    rdspls = malloc(p*sizeof(int));
+
+    MPI_Alltoall(scounts, 1, MPI_INT, rcounts, 1, MPI_INT, MPI_COMM_WORLD);
+    rdspls[0] = 0;
+    for ( i = 1; i < p; i++ )
+        rdspls[i] = rdspls[i - 1] + rcounts[i - 1];
+    const int local_count = rdspls[p - 1] + rcounts[p -1];
+    /*printf("\nHello from %d, my local count is %d!\n\n", my_rank, local_count);*/
+    local_array = malloc(local_count*sizeof(double));
+    MPI_Alltoallv(bucketlist, scounts, dspls, MPI_DOUBLE, local_array, rcounts,
+            rdspls, MPI_DOUBLE, MPI_COMM_WORLD);
+
+    t_local_sort = MPI_Wtime();
+    t_distribute = t_local_sort - t_distribute;
+    // do the local sorting
+    qsort_dbls(local_array, local_count);
+
+    t_gathering = MPI_Wtime();
+    t_local_sort = t_gathering - t_local_sort;
+
+    // combine the results
+    MPI_Gather(&local_count, 1, MPI_INT, rcounts, 1, MPI_INT, 0,
+            MPI_COMM_WORLD);
+
+    if ( my_rank == 0 )
+    {
+        for ( i = 1; i < p; i++ )
+            rdspls[i] = rdspls[i - 1] + rcounts[i - 1];
+        result = malloc(n*sizeof(double));
+    }
+    MPI_Gatherv(local_array, local_count, MPI_DOUBLE, result,
+            rcounts, rdspls, MPI_DOUBLE, 0, MPI_COMM_WORLD);
+
+    t_gathering = MPI_Wtime() - t_gathering;
+
+    double *t_generate_array;
+    double *t_binning_array;
+    double *t_distribute_array;
+    double *t_local_sort_array;
+    double *t_gathering_array;
+
+    if ( my_rank == 0 )
+    {
+        t_total = MPI_Wtime() - t_total;
+        t_generate_array = malloc(p*sizeof(double));
+        t_binning_array = malloc(p*sizeof(double));
+        t_distribute_array = malloc(p*sizeof(double));
+        t_local_sort_array = malloc(p*sizeof(double));
+        t_gathering_array = malloc(p*sizeof(double));
+    }
+
+    MPI_Gather(&t_generate, 1, MPI_DOUBLE, t_generate_array, 1, MPI_DOUBLE,
+            0, MPI_COMM_WORLD);
+    MPI_Gather(&t_binning, 1, MPI_DOUBLE, t_binning_array, 1, MPI_DOUBLE,
+            0, MPI_COMM_WORLD);
+    MPI_Gather(&t_distribute, 1, MPI_DOUBLE, t_distribute_array, 1, MPI_DOUBLE,
+            0, MPI_COMM_WORLD);
+    MPI_Gather(&t_local_sort, 1, MPI_DOUBLE, t_local_sort_array, 1, MPI_DOUBLE,
+            0, MPI_COMM_WORLD);
+    MPI_Gather(&t_gathering, 1, MPI_DOUBLE, t_gathering_array, 1, MPI_DOUBLE,
+            0, MPI_COMM_WORLD);
+
+    MPI_Finalize();
+
+    // Check result
+    if ( my_rank == 0 )
+    {
+        /*printf("Result is: [");*/
+        /*for ( i = 0; i < n - 1; i++ )*/
+            /*printf("%f, ", result[i]);*/
+        /*printf("%f]\n", result[n-1]);*/
+        /*printf("Local is: [");*/
+        /*for ( i = 0; i < local_count - 1; i++ )*/
+            /*printf("%f, ", local_array[i]);*/
+        /*printf("%f]\n", local_array[n-1]);*/
+
+        // Only print someting here if the output is incorrect, I don't want
+        // spam in my output files
+        for ( i = 0; i < n - 1; i++ )
+            if ( result[i] > result[i+1] )
+            {
+                printf("OUTPUT IS INCORRECT!\n");
+                return 1;
+            }
+
+        double generate_min, generate_avg, generate_max;
+        double binning_min, binning_avg, binning_max;
+        double distribute_min, distribute_avg, distribute_max;
+        double local_sort_min, local_sort_avg, local_sort_max;
+        double gather_min, gather_avg, gather_max;
+
+        get_min_avg_max(t_generate_array, p, &generate_min,
+                &generate_avg, &generate_max);
+        get_min_avg_max(t_binning_array, p, &binning_min,
+                &binning_avg, &binning_max);
+        get_min_avg_max(t_distribute_array, p, &distribute_min,
+                &distribute_avg, &distribute_max);
+        get_min_avg_max(t_local_sort_array, p, &local_sort_min,
+                &local_sort_avg, &local_sort_max);
+        get_min_avg_max(t_gathering_array, p, &gather_min,
+                &gather_avg, &gather_max);
+
+        printf("Time Report:\n");
+        printf("============\n");
+        printf("Total:      %.6f\n", t_total);
+        printf("Generate:\n");
+        printf("       Min: %.6f\n", generate_min);
+        printf("       Avg: %.6f\n", generate_avg);
+        printf("       Max: %.6f\n", generate_max);
+        printf("Binning:\n");
+        printf("       Min: %.6f\n", binning_min);
+        printf("       Avg: %.6f\n", binning_avg);
+        printf("       Max: %.6f\n", binning_max);
+        printf("Distribute:\n");
+        printf("       Min: %.6f\n", distribute_min);
+        printf("       Avg: %.6f\n", distribute_avg);
+        printf("       Max: %.6f\n", distribute_max);
+        printf("Local Sort:\n");
+        printf("       Min: %.6f\n", local_sort_min);
+        printf("       Avg: %.6f\n", local_sort_avg);
+        printf("       Max: %.6f\n", local_sort_max);
+        printf("Gathering:\n");
+        printf("       Min: %.6f\n", gather_min);
+        printf("       Avg: %.6f\n", gather_avg);
+        printf("       Max: %.6f\n", gather_max);
+    }
+    return 0;
 }
+
