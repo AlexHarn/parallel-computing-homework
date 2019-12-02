165469068bdda2f9610e843388cd3bc447e8ef7f
Alexander Harnisch <harnisc6@dev-intel14.i>
2019-12-02 12:59:15 -0500

    Homework 6:     Added job scripts for Part 1.

---
+++ homework/6/part_1/submit_jobs.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+template=`cat job_script.template`
+
+wall_time="00:15:00"
+memory="100G"
+
+for p in {14..140..14}
+do
+    nodes=$(( ($p + 14)/28 ))
+    for n in 100000000 500000000 2000000000
+    do
+        temp="${template//<+P+>/$p}"
+        temp="${temp//<+NODES+>/$nodes}"
+        temp="${temp//<+N+>/$n}"
+        temp="${temp//<+WALL_TIME+>/$wall_time}"
+        temp="${temp//<+MEMORY+>/$memory}"
+        echo "$temp" > "$p"_"$n".sb
+        sbatch "$p"_"$n".sb
+    done
+done
